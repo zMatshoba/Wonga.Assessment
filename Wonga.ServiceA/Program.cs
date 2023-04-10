3dde7b0d23c02542e2df77e35f18e2f3439db2e1
zMatshoba <zola.matshoba@gmail.com>
2023-04-10 18:24:45 +0200

    added RabbitMQ NugetPackages, Created the MessageSender Class, and also created the MessageConsolidate methode.

--- Wonga.ServiceA/Program.cs
+++ Wonga.ServiceA/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Wonga.ServiceA // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello what is your name?");
+
+            var name = Console.ReadLine();
+
+
+        }
+
+        private static string MessageConsolidation(string name) {
+
+            if (!String.IsNullOrEmpty(name))
+                return "Hello my name is {1} " + name;
+            else
+                return "Exit";
+        }
+    }
+}
+
+
