2d40757aaaacb64e7d9daf9595ab6990b4300d61
zMatshoba <zola.matshoba@gmail.com>
2023-04-12 22:21:22 +0200

    Completed Service A and Service B, Added Unit Tests for Service B

--- Wonga.ServiceA/Program.cs
+++ Wonga.ServiceA/Program.cs
@@ -6,19 +6,26 @@ namespace Wonga.ServiceA // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            const string QUEUE_NAME = "wonga-queue";
+            MessageSender send = new(QUEUE_NAME);
+
             Console.WriteLine("Hello what is your name?");
 
-            var name = Console.ReadLine();
+            var message = MessageConsolidation(Console.ReadLine());
 
+            var result = send.SendMessageAsync(message) ? "Message Sent" : "Failed to send";
 
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
 
-        private static string MessageConsolidation(string name) {
+        private static string MessageConsolidation(string? name)
+        {
 
             if (!String.IsNullOrEmpty(name))
-                return "Hello my name is {1} " + name;
+                return "Hello my name is, " + name;
             else
-                return "Exit";
+                return string.Empty;
         }
     }
 }
--- Wonga.UnitTest/UnitTest1.cs
+++ Wonga.UnitTest/UnitTest1.cs
@@ -1,18 +1,69 @@
 using NUnit.Framework;
 
+using Wonga.ServiceB;
+
 namespace Wonga.UnitTest
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void ReturnMessageIsCorrect()
         {
-            Assert.Pass();
+            //Arrange 
+            string TestName = "Hello my name is, Zola";
+
+            //Act
+            var result = ValidateMessage.Validate(TestName);
+
+            //Assert
+            Assert.IsTrue(result);
         }
+
+        [Test]
+        public void ReturnMessageIsNotCorrect()
+        {
+            //Arrange 
+            string TestName = "Hello my name is Zola";
+
+            //Act
+            var result = ValidateMessage.Validate(TestName);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+
+        [Test]
+        public void CanSplit()
+        {
+            //Arrange 
+            string TestName = "Hello my name is, Zola";
+            string answer = "Zola";
+            //Act
+            var result = ValidateMessage.MessageSplit(TestName);
+
+            //Assert
+            Assert.Pass(result);
+        }
+
+        [Test]
+        public void CantSplit()
+        {
+            //Arrange 
+            string TestName = "Hello my name is Zola";
+            string answer = "Zola";
+            //Act
+            var result = ValidateMessage.MessageSplit(TestName);
+
+            //Assert
+            Assert.IsEmpty(result);
+        }
+
     }
 }
